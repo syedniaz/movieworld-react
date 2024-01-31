f614a912a1e3d079f32a6d8d420034118417f4cb
syedniaz <snm051110@gmail.com>
2024-02-01 03:22:14 +0600

    yay first react app

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# movieworld-react
+# Movie World - React
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 A responsive movie database website built using React.
+
+![Screenshot (000)](./ss0.png)
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+
+import './App.css';
+import SearchIcon from './search.svg'
+
+const API_URL = 'https://www.omdbapi.com/?apikey=2032bd1f'
+
+
+const App = () => {
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(''); 
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('Batman');
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieWorld</h1>
+
+      <div className="search">
+        <input 
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img 
+          src={SearchIcon}
+          alt="Search Icon"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0
+        ? (
+          <div className="container">
+            {movies.map( (movie) => (
+              <MovieCard movie={movie}/>
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )
+      }
+
+
+    </div>
+  );
+}
+
+export default App;
